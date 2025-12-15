def7ea43be33809e972a8f0ce167c153b9b5c5f0
Oliver Öhrström <xzereha@live.se>
2025-12-15 13:09:57 +0100

    Added composite id class

--- src/main/java/com/example/models/OrderItem.java
+++ src/main/java/com/example/models/OrderItem.java
@@ -8,7 +8,7 @@ public class OrderItem {
     @EmbeddedId
     private OrderItemId id = new OrderItemId();
 
-    //Relations
+    // Relations
     @ManyToOne
     @MapsId("order_id")
     @JoinColumn(name = "order_id")
@@ -34,12 +34,29 @@ public class OrderItem {
         this.lineTotal = lineTotal;
     }
 
-    public OrderItem(OrderItemId OrderItemId, Order order, Product product, int quantity, double unitPrice, double lineTotal){
+    public OrderItem(OrderItemId OrderItemId, Order order, Product product, int quantity, double unitPrice,
+            double lineTotal) {
         this.id = OrderItemId;
         this.order = order;
         this.product = product;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
         this.lineTotal = lineTotal;
+    }
+
+    @Embeddable
+    class OrderItemId implements java.io.Serializable {
+        @Column(name = "order_id")
+        private Long order_id;
+        @Column(name = "product_id")
+        private Long product_id;
+
+        public OrderItemId() {
+        }
+
+        public OrderItemId(Long order_id, Long product_id) {
+            this.order_id = order_id;
+            this.product_id = product_id;
+        }
     }
 }
