504acaa180d423b38ad6f2235da9935a58c06e11
Oliver Öhrström <xzereha@live.se>
2025-12-17 16:53:20 +0100

    Created missing repositories

---
+++ src/main/java/com/example/respoitories/OrdersRepository.java
@@ -0,0 +1,9 @@
+package com.example.respoitories;
+
+import com.example.models.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Integer> {
+}
