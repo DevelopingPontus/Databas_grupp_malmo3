dc8f2497c6c06bdca58be5b3ce4f781e4cafe090
DevelopingPontus <ipont35@gmail.com>
2025-12-19 11:42:49 +0100

    Added Test file for CartService that is in progress. Change in Orders, total set to BigDecimal

--- src/main/java/com/example/models/Orders.java
+++ src/main/java/com/example/models/Orders.java
@@ -1,24 +1,22 @@
 package com.example.models;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-
 @Entity
 @Table(name = "orders")
 public class Orders {
-    // Attribut
-    public enum OrderStatus {
-        NEW, PAID, CANCELLED
-    }
-
+    // Relations
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final Set<OrderItem> orderItems = new HashSet<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ORDER_ID")
@@ -28,18 +26,12 @@ public class Orders {
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     private OrderStatus status;
     @Column(columnDefinition = "numeric(10,2) default 0.0")
-    private double total;
+    private BigDecimal total;
     @Column(nullable = false)
     @CreationTimestamp
     private Timestamp createdAt;
-
-    // Relations
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<OrderItem> orderItems = new HashSet<>();
-
     @OneToOne(mappedBy = "orders")
     private Payment payment;
-
     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
@@ -50,12 +42,12 @@ public class Orders {
 
     public Orders(Customer customer) {
         this.customer = customer;
-        this.total = 0;
+        this.total = BigDecimal.valueOf(0);
         this.status = OrderStatus.NEW;
         customer.addOrder(this);
     }
 
-    public Orders(OrderStatus status, double total, Timestamp createdAt) {
+    public Orders(OrderStatus status, BigDecimal total, Timestamp createdAt) {
         this.status = status;
         this.total = total;
         this.createdAt = createdAt;
@@ -85,11 +77,11 @@ public class Orders {
         this.status = status;
     }
 
-    public double getTotal() {
+    public BigDecimal getTotal() {
         return total;
     }
 
-    public void setTotal(double total) {
+    public void setTotal(BigDecimal total) {
         this.total = total;
     }
 
@@ -123,5 +115,10 @@ public class Orders {
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
+    }
+
+    // Attribut
+    public enum OrderStatus {
+        NEW, PAID, CANCELLED
     }
 }
---
+++ src/test/java/org/example/services/CartServiceTest.java
@@ -0,0 +1,105 @@
+package org.example.services;
+
+import com.example.models.*;
+import com.example.respoitories.OrderItemRepository;
+import com.example.respoitories.OrderRepository;
+import com.example.respoitories.PaymentRepository;
+import com.example.respoitories.ProductRepository;
+import com.example.services.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+@ExtendWith(MockitoExtension.class)
+public class CartServiceTest {
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private OrderItemRepository orderItemRepository;
+    @Mock
+    private PaymentRepository paymentRepository;
+    @Mock
+    private CustomerService customerService;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private OrderService orderService;
+    @Mock
+    private InventoryService inventoryService;
+    @Mock
+    private PaymentService paymentService;
+
+    @InjectMocks
+    private CartService cartService;
+
+    private Customer customer;
+    private Product product;
+    private Orders orderEmpty;
+    private Orders orderWithItem;
+    private OrderItem orderItem;
+    private Payment payment;
+
+    @BeforeEach
+    void setup() {
+        customer = new Customer("p@ntu.se", "Pontus");
+        product = new Product("ThisSKU", "Billys Pizza", "Mumma!", BigDecimal.valueOf(15), true);
+        orderEmpty = new Orders(customer);
+        orderWithItem = new Orders(customer);
+        orderWithItem.setTotal();
+    }
+
+    //Add to cart tests
+    @Test
+    void shouldAddToCartAndUpdateOrder() {
+
+    }
+
+    @Test
+    void shouldThrowWhenAddingMoreThenThereAreInStock() {
+
+    }
+
+    //Create or update order item tests
+    @Test
+    void shouldMakeNewOrderItemWithParametersAndSave() {
+
+    }
+
+    @Test
+    void shouldNotMakeNewOrderItem() {
+
+    }
+
+    @Test
+    void shouldFindCurrentOrderItemUpdateItAndSaveIt() {
+
+    }
+
+    //Update order total tests
+    @Test
+    void shouldUpdateOrderTotal() {
+
+    }
+
+    //Remove from cart tests
+    @Test
+    void shouldUpdateOrderQuantity() {
+
+    }
+
+    @Test
+    void shouldThrowWhenRemovingMoreQuantityOfItemThenThereIsInCart() {
+
+    }
+
+    //Get cart Items tests
+    @Test
+    void shouldReturnEverythingInACart() {
+
+    }
+}
