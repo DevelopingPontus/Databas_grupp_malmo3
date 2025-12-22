05df2eab59dde8393200467c93efd9e0b86e984a
DevelopingPontus <ipont35@gmail.com>
2025-12-22 12:40:32 +0100

    Test file in progress.

--- src/test/java/com/example/services/CartServiceJPATest.java
+++ src/test/java/com/example/services/CartServiceJPATest.java
@@ -5,11 +5,7 @@ import com.example.repositories.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -19,10 +15,6 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
-@Import(JsonService.class)
 class CartServiceJPATest {
 
     @Autowired
