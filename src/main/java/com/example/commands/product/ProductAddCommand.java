da90badb00e658c316c6a5e07c6031ac2f685610
ZaCc42 <mario.zalac@chasacademy.se>
2025-12-22 23:37:08 +0100

    fix

--- src/main/java/com/example/commands/product/ProductAddCommand.java
+++ src/main/java/com/example/commands/product/ProductAddCommand.java
@@ -12,7 +12,6 @@ import java.math.BigDecimal;
 public class ProductAddCommand implements Runnable {
     private final ProductService productService;
 
-    // String sku, String name, String description, double price
     @Parameters(index = "0", description = "Product SKU")
     private String sku;
 
@@ -25,20 +24,17 @@ public class ProductAddCommand implements Runnable {
     @Parameters(index = "3", description = "Product description", arity = "0..1")
     private String description;
 
-    @Parameters(index = "4", description = "Product category", arity = "0..1")
-    private String category;
-
     public ProductAddCommand(ProductService productService) {
         this.productService = productService;
     }
 
     public void run() {
         var product = productService.addProducts(sku, name, description, price);
-        System.out.printf(" SKU:%s | Name:%s | Description:%s | Price:%.2f | Active:%b%n",
+        System.out.printf(" SKU:%s | Name:%s | Price:%.2f | Description:%s | Active:%b%n",
                 product.getSku().toLowerCase(),
                 product.getName(),
-                product.getDescription(),
                 product.getPrice(),
+                product.getDescription(),
                 product.isActive()
         );
     }
